e4c97057b37ecda5116a78622d6c55f3c5c58c93
theMladyPan <stanislav@rubint.sk>
2023-03-02 15:57:44 +0100

    added cli, vbump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="xerxes-protocol",
-    version="0.1.4",
+    version="1.4.1",
     author="Stanislav Rubint",
     author_email="stanislav@rubint.sk",
     description="Python implementation for xerxes-protocol",
@@ -19,5 +19,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: Unix",
     ],
+    entry_points = {
+        'console_scripts': ['xerxes_config=xerxes_protocol.cli_util:address_config'],
+    },
     python_requires='>=3.8',
 )
